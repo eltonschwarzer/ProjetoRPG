212cfabd624dc482825440c153a119f1fc269804
JONAS-NOTE\Jonas <jonas@fortalezatec.com.br>
2018-07-31 22:15:35 -0300

    Commit com ajustes iniciais

--- ProjetoRPG/src/projetorpg/ProjetoRPG.java
+++ ProjetoRPG/src/projetorpg/ProjetoRPG.java
@@ -10,7 +10,12 @@ public class ProjetoRPG {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //INICIALIZACOES DO JOGO
+        System.out.println("Inicialização...");
         
+        //ABERTURA DA TELA PRINCIPAL
+        Principal tela = new Principal();
+        tela.setVisible(true);
     }
     
 }
